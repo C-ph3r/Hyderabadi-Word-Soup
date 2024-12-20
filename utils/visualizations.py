9232c8af398e9c03b0c765ba6dabeeafc6a6da08
C-ph3r <margaridaimsardinha@gmail.com>
2024-12-20 23:09:36 +0000

    final changes?

--- utils/visualizations.py
+++ utils/visualizations.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from collections import defaultdict, Counter
+from tqdm import tqdm
+import numpy as np
 
 def plot_term_frequency(df, nr_terms, df_name, show=True):
     
@@ -16,3 +20,36 @@ def plot_term_frequency(df, nr_terms, df_name, show=True):
     plt.close()
 
     return fig
+
+
+def cooccurrence_matrix_window_generator(preproc_sentences, window_size):
+
+    co_occurrences = defaultdict(Counter)
+
+    # Compute co-occurrences
+    for sentence in tqdm(preproc_sentences):
+        for i, word in enumerate(sentence):
+            for j in range(max(0, i - window_size), min(len(sentence), i + window_size + 1)):
+                if i != j:
+                    co_occurrences[word][sentence[j]] += 1
+
+    #ensure that words are unique
+    unique_words = list(set([word for sentence in preproc_sentences for word in sentence]))
+
+    # Initialize the co-occurrence matrix
+    co_matrix = np.zeros((len(unique_words), len(unique_words)), dtype=int)
+
+    # Populate the co-occurrence matrix
+    word_index = {word: idx for idx, word in enumerate(unique_words)}
+    for word, neighbors in co_occurrences.items():
+        for neighbor, count in neighbors.items():
+            co_matrix[word_index[word]][word_index[neighbor]] = count
+
+    # Create a DataFrame for better readability
+    co_matrix_df = pd.DataFrame(co_matrix, index=unique_words, columns=unique_words)
+
+    co_matrix_df = co_matrix_df.reindex(co_matrix_df.sum().sort_values(ascending=False).index, axis=1)
+    co_matrix_df = co_matrix_df.reindex(co_matrix_df.sum().sort_values(ascending=False).index, axis=0)
+
+    # Return the co-occurrence matrix
+    return co_matrix_df
